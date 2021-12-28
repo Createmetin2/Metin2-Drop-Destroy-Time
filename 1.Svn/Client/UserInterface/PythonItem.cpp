6ba25c883e6196f4569a735e99f18c16d74fd685
blackdragonx61 <33325253+blackdragonx61@users.noreply.github.com>
2021-12-28 22:12:19 +0300

    Update PythonItem.cpp

--- 1.Svn/Client/UserInterface/PythonItem.cpp
+++ 1.Svn/Client/UserInterface/PythonItem.cpp
@@ -1,10 +1,34 @@
 //Find
-		itor->second->Update();
+void CPythonItem::Update(const POINT& c_rkPtMouse)
+{
+	...
+}
 
-///Add
+///Change Function
+void CPythonItem::Update(const POINT& c_rkPtMouse)
+{
 #if defined(__BL_DROP_DESTROY_TIME__)
-		CPythonTextTail::Instance().SetItemTextTailDestroyTime(itor->first, itor->second->tDestroyTime);
+	static DWORD s_dwLastUpdate = 0;
+	const bool bCanUpdate = (CTimer::Instance().GetCurrentMillisecond() - s_dwLastUpdate) >= 1000;
 #endif
+	
+	TGroundItemInstanceMap::iterator itor = m_GroundItemInstanceMap.begin();
+	for(; itor != m_GroundItemInstanceMap.end(); ++itor)
+	{
+		itor->second->Update();
+#if defined(__BL_DROP_DESTROY_TIME__)
+		if (bCanUpdate)
+			CPythonTextTail::Instance().SetItemTextTailDestroyTime(itor->first, itor->second->tDestroyTime);
+#endif
+	}
+
+#if defined(__BL_DROP_DESTROY_TIME__)
+	if (bCanUpdate)
+		s_dwLastUpdate = CTimer::Instance().GetCurrentMillisecond();
+#endif
+
+	m_dwPickedItemID=__Pick(c_rkPtMouse);
+}
 
 //Find
 	pGroundItemInstance->dwVirtualNumber = dwVirtualNumber;
